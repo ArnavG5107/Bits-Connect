956dc35e6976ae61656e1c51bc3eb893be9d506b
ArnavG5107 <arnavgupta.svt@gmail.com>
2025-03-12 22:03:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Home from "./Components/Home/Home.js";
+import Navbar from "./Components/Navbar.js";
+import Footer from "./Components/Footer.js";
+import ContactPage from "./Components/Contact.js";
+import RegistrationPage from "./Components/Register.js";
+import SignInPage from "./Components/SignIn.js";
+
+function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  // Function to handle navigation
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Render the appropriate component based on currentPage state
+  const renderPage = () => {
+    switch (currentPage) {
+      case "contact":
+        return <ContactPage />;
+      case "Register":
+        return <RegistrationPage />;
+      case "home":
+      default:
+        return <Home />;
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Navbar navigateTo={navigateTo} />
+        {renderPage()}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
